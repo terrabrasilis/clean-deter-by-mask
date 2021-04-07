ad94f2a48978905c88d4cc61ec72e2ffe43fbbaa
André Carvalho <afacarvalho@yahoo.com.br>
2021-04-07 17:11:14 -0300

    Adjusts to fix path of postgis client binaries

--- script_defines.sh
+++ script_defines.sh
@@ -2,7 +2,7 @@
 export PGPASSWORD
 PG_CON_BKP="-U $PG_USER -h $HOST -p $PG_PORT"
 PG_CON="-d $PG_DATABASE $PG_CON_BKP"
-PG_BIN="/usr/bin"
+PG_BIN="/usr/local/bin"
 BASE_DIR=$(pwd)
 ACT_DATE=$(date '+%d_%m_%y')
 LOGFILE="$BASE_DIR/stack_trace_$ACT_DATE.log"
